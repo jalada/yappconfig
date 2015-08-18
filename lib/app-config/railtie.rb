d7f16fda90cba22e119fe5ff8d4bc8d9f351b21a
David Somers <david@jalada.co.uk>
2015-08-18 09:06:06 +0100

    Support for dotenv-rails now we use before_configuration too

--- lib/app-config/railtie.rb
+++ lib/app-config/railtie.rb
@@ -3,6 +3,13 @@ require 'rails'
 module AppConfig
   class Railtie < Rails::Railtie
     config.before_configuration do
+
+      # AppConfig can be used in conjunction with dotenv, and as per their
+      # code, we are running `.load` ahead of them.
+      if defined?(Dotenv::Railtie) && Dotenv::Railtie.respond_to?(:load)
+        Dotenv::Railtie.load
+      end
+
       AppConfig.configure do |config|
         config.config_file = Rails.root.join("config", "config.yml")
         config.environment = Rails.env
